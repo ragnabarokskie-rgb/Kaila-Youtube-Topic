fc5615634b684e9c344f388622fdff67afc031fa
ragnabarokskie-rgb <ragnabarokskie@gmail.com>
2026-07-05 03:04:46 -0400

    July 5 2026 2nd Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,19 @@
-const CACHE='yt-topics-v3';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','index.html']))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE = 'yt-topics-v4';
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(['/', 'index.html']))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(response => {
+        let rc = response.clone();
+        caches.open(CACHE).then(c => c.put(e.request, rc));
+        return response;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
