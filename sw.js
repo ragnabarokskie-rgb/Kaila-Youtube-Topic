be69f51b08f607682dac24518b75818f9eb4634c
ragnabarokskie-rgb <ragnabarokskie@gmail.com>
2026-07-05 03:33:27 -0400

    july 5 2026 3rd Update sw.js

--- sw.js
+++ sw.js
@@ -6,6 +6,18 @@ self.addEventListener('install', e => {
   );
 });
 
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE) return caches.delete(key);
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch', e => {
   e.respondWith(
     fetch(e.request)
